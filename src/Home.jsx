26be20219d0a9cf50423bb2e60ac3347bf018125
Mohammadi <rs.mohammadi77@gmail.com>
2025-12-17 11:02:27 +0330

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import TransactionList from "./layout/TransactionList/TransactionList.jsx";
 import "./Home.css";
 function Home() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="div-Home">
       <TransactionList />
